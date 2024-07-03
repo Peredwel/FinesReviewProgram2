e80bc59f93e29a1eb123a2d7520d5c459d792c9f
CS <csulliv4@uottawa.ca>
2024-07-03 14:36:33 -0400

    changed main to let script be run with directory argument

--- main.py
+++ main.py
@@ -1,62 +1,63 @@
 import os
 import csv
 import operator
-import pandas as pd
-import openpyxl
+import argparse
+import re
 
 HEADERS = ['lender_institution', 'borrower_institution', 'full_name', 'user_email', 'expiry_date', 'remaining_amount', 'active_loan_count']
 
-#This function checks the csv files provided in InputAFNFiles_csv and ensures they are, in fact, Analytics reports
-#TODO: Ask Alex if we have to add a user input option to check graduation date (and ensure all the reports added ARE, in
+#Function to pull path for files as commandline argument
+def setUp():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', type=str, help='directory from which to run this script')
+    #print(parser.parse_args().directory)
+    return parser.parse_args().directory
+
+# A function to create a list of matching files within a repository
+# Returns a list of filenames with absolute paths
+def match(directory):
+    matches = []
+    # Step 1: Obtain all directories matching the pattern:
+    for root, dirnames, filenames in os.walk(directory):
+        for dirname in dirnames:
+            pattern = re.compile("al_*")
+            if pattern.match(dirname):
+                # print(dirname)
+                # print(root)
+                # print(root + '/' + dirname)
+                holdingdir = root + '/' + dirname
+                # Step 2: Grab all the files in this directory and shove them in the matches list.
+                for file in os.listdir(holdingdir):
+                    matches.append(holdingdir + '/' + os.fsdecode(file))
+    #print(matches)
+    return matches
+
+#This function checks the csv files output in match()
+#Ask Alex if we have to add a user input option to check graduation date (and ensure all the reports added ARE, in
 # fact, for the set of users graduating that semester, etc.
-
-def checkcsv(filename):
-    with open('InputAFNFiles_csv/' + filename, encoding='latin-1') as csv_file1:
-        csv_reader = csv.reader(csv_file1, delimiter=',')
-        headers = next(csv_reader)
-        headers[0] = headers[0].replace('ï»¿', '')  # remove byte order mark
-        if headers != HEADERS:
-            error="The headers in this file (" + filename + ") do not match those of the expected analytics report.  Did you provide the correct file?"
-        else:
-            error=""
-    csv_file1.close()
-    return error
-
-def convertcsvtoxlsx(filename):
-    read_file = pd.read_excel('InputAFNFiles_csv/' + filename)
-    newfilename = filename.split('.')[0] + ".csv"
-    read_file.to_csv('InputAFNFiles_csv/'+ newfilename, index=None, header=True)
-    #delete xlsx file
-    os.remove('InputAFNFiles_csv/'+ filename)
-    error = checkcsv(newfilename)
-    return error
-
-def checkFiles():
+# Accept list of matches from match() and check to make sure they're actually the correct format, etc.
+def checkFileHeaders(matcheslist):
     errorfile = open("Logs/errorfile.txt", 'a')
-    noErrorInAnyFile = True
-    for file in os.listdir('InputAFNFiles_csv'):
-        filename = os.fsdecode(file)
-        filetype = checkFileExtension(filename)
-        if filetype == 'csv':
-            errorInFile = checkcsv(filename)
-        elif filetype == 'xlsx':
-            errorInFile = convertcsvtoxlsx(filename)
+    #print(matcheslist)
+    correctfiles = []
+    for filename in matcheslist:
+        filetype = os.path.splitext(filename)[-1]
+        #print(filetype)
+        if filetype == '.csv':
+            with open(filename, encoding='latin-1') as csv_file1:
+                csv_reader = csv.reader(csv_file1, delimiter=',')
+                headers = next(csv_reader)
+                headers[0] = headers[0].replace('ï»¿','') #remove byte order mark
+                if headers != HEADERS:
+                    errorfile.write(filename + "\t The headers in this file do not match those of the expected analytics report.  Did you provide the correct file?\n")
+                   # TODO: add conversion option from xlsx
+                correctfiles.append(filename)
+            csv_file1.close()
         else:
-            errorInFile = "The extension of " + filename + " does not match any accepted format.  Please provide a csv or xlsx file and try again"
-        if errorInFile:
-            print(errorInFile)
-            errorfile.write(errorInFile)
-            noErrorInAnyFile = False
-    return noErrorInAnyFile
-
-def checkFileExtension(filename):
-    components = filename.split(".")
-    filextension = ''
-    if len(components) < 2:
-        filextension = 'No file extension'
-    else:
-        filextension = components[1]
-    return filextension
+            errorfile.write(filename + "\t The extension of this file is not as expected.  Did you provide the correct file?\n")
+    errorfile.close()
+    #print(correctfiles)
+    return correctfiles
 
 # This function creates empty report files for each school
 # Returns a dictionary relating which file corresponds to which institution
@@ -73,12 +74,13 @@ def createReportFiles():
 
 # Receives the filedict created in the first function
 # Iterates through the input files and sorts all borrowers from one institution into that institution's file
-
+# Also receives the list of matches created in match(directory)
 def populateReport(filedict):
-    for file in os.listdir('InputAFNFiles_csv'):
-        filename = os.fsdecode(file)
+    correctfiles = checkFileHeaders(match(setUp()))
+    #print(correctfiles)
+    for filename in correctfiles: # provides string filename as absolute path
         line_count = 0
-        with open('InputAFNFiles_csv/' + filename, encoding='latin-1') as csv_file1:
+        with open(filename, encoding='latin-1') as csv_file1:
             csv_reader = csv.reader(csv_file1, delimiter=',')
             for row in csv_reader:
                 if line_count > 0:
@@ -107,15 +109,20 @@ def sortReportsByEmail():
             csv_reader = csv.reader(csv_file1, delimiter='\t')
             with open('SortedFiles/' + filename, "w", newline='', encoding='latin-1') as writefile:
                 csv_writer = csv.writer(writefile, delimiter='\t')
+                # headers = next(csv_reader)
+                # blank = next(csv_reader)
                 sorted_rows = sorted(csv_reader, key=operator.itemgetter(3))
-                csv_writer.writerow(HEADERS)
+                csv_writer.writerow(["lender_institution","borrower_institution","full_name","user_email","expiry_date","remaining_amount","active_loan_count"])
                 csv_writer.writerows(sorted_rows)
 
+
 def main():
-    noErrorInAnyFiles = checkFiles()
-    if noErrorInAnyFiles:
-        mapSchoolToFile = createReportFiles()
-        populateReport(mapSchoolToFile)
-        sortReportsByEmail()
+    mapSchoolToFile = createReportFiles()
+    populateReport(mapSchoolToFile)
+    sortReportsByEmail()
+    #checkFileHeaders(['./al_Trent/DSpaceNotes.odt', './al_Trent/Trent.csv', './al_uOttawa/Ottawa.csv', './InputAFNFiles_csv/al_Guelph/York.csv'])
+
 
 main()
+
+
