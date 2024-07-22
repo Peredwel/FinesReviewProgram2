2d4e851727ceb3ded63b960465017d358901f38c
CS <csulliv4@uottawa.ca>
2024-07-22 15:16:09 -0400

    First commit

--- conda/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-conda env create -f environment.yml
-conda activate afnfines
-echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "1. copy your csv file to the Input..."
-echo "2. python main.py "
-echo "============================================================="
